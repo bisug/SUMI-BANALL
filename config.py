63c296337f28e2f9f769a49817aebfad25ca4c3a
Bisu G <lannsnolo@gmail.com>
2025-07-27 10:03:57 +0545

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", 26208465))
+API_HASH = os.getenv("API_HASH", "bd4e2fe9c30486282417cdf9a93333b2")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "dear_bot")
+OWNER_ID = int(os.getenv("OWNER_ID", 7804972365)) 
