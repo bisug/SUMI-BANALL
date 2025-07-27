ffa1870bbdc0fa90b38857f77d4a282db8776ac8
Bisu G <lannsnolo@gmail.com>
2025-07-27 10:19:54 +0545

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 API_ID = int(os.getenv("API_ID", 26208465))
 API_HASH = os.getenv("API_HASH", "bd4e2fe9c30486282417cdf9a93333b2")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "dear_bot")
+BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+BOT_USERNAME = os.getenv("BOT_USERNAME", None)
 OWNER_ID = int(os.getenv("OWNER_ID", 7804972365)) 
